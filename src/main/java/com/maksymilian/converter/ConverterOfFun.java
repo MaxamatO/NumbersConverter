3ac07c3168c2523f2cb2d76c1bd81c7f3cd4ed69
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-04-04 19:56:21 +0200

    Text/numbers to binary works

--- src/main/java/com/maksymilian/converter/ConverterOfFun.java
+++ src/main/java/com/maksymilian/converter/ConverterOfFun.java
@@ -1,4 +1,7 @@
+package com.maksymilian.converter;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ConverterOfFun {
@@ -6,10 +9,8 @@ public class ConverterOfFun {
     public static void main(String[] args) {
 
         ConverterOfFun converterOfFun = new ConverterOfFun();
-        String textToConvert = "Example   string";
-//        System.out.println(converterOfFun.toBinary("Example string     "));
+        String textToConvert = "Text and numbers to binary work!@#!@";
         System.out.println(converterOfFun.toBinary(textToConvert));
-//        System.out.println(base62.removeSpaces(base62.toBinary(textToConvert)));
     }
 
     public int toDecimal(){
@@ -29,17 +30,16 @@ public class ConverterOfFun {
     public List<String> toBinary(int numberToChange){
 
         List<String> result = new ArrayList<>();
-        boolean changed = false;
 
         if(numberToChange==1){
             result.add("00000001");
-            changed=true;
+            return result;
         }
-
-        while(!changed){
+        // Binary conversion process
+        // If something is divisible by 2, append 0, else append 1
+        while(true){
             if(numberToChange==1){
                 result.add("1");
-                changed=true;
                 break;
             }
             if(numberToChange%2==0){
@@ -50,11 +50,14 @@ public class ConverterOfFun {
             }
             numberToChange = numberToChange/2;
         }
+
+        // Reverse the list, so the binary goes in the right order
         result=reverse(result);
         return result;
     }
 
-    public List<String> reverse(List<String> data){
+    // Loop through an array and initialize a new one that is reversed
+    private List<String> reverse(List<String> data){
         List<String> result = new ArrayList<>();
         for (int i = data.size(); i > 0; i--){
             result.add((data.get(i-1)));
@@ -62,37 +65,44 @@ public class ConverterOfFun {
         return result;
     }
 
-//    protected String convertToString(List<String> data){
-////        System.out.println(makeEightDigits(data));
-//        String result ="";
-//        for (int i =0; i < data.size(); i++){
-//            result += data.get(i);
-//            result += " ";
-//        }
-//
-////        result = removeSpaces(result);
-//
-//        return result;
-//    }
+    private String convertToString(List<String> data){
+        StringBuilder result = new StringBuilder();
+        for (String datum : data) {
+            result.append(datum);
+            result.append(" ");
+        }
+        return result.toString();
+    }
 
+    /**
+     * To convert string to binary, we need to create an array of chars that the input string contains,
+     * create an array of integers, that stores ASCII values of specific chars,
+     * change these ASCII values into binary and return a list of binaries
+     */
     public String toBinary(String textToChange){
 
         List<Character> chars = new ArrayList<>();
         List<Integer> ints = new ArrayList<>();
-        StringBuilder result = new StringBuilder();
+        List<String> result = new ArrayList<>();
+
         for(char c:textToChange.toCharArray()){
             chars.add(c);
         }
         for (Character aChar : chars) {
             ints.add((int) aChar);
+
         }
-        for (int i : ints){
-            result.append(toBinary(i));
+        for (Integer i : ints){
+            // Append a binary representation of an ASCII value to the String array
+            result.add(convertToString((toBinary(i))));
+
         }
-        return removeSpaces(result.toString());
+        // Convert List to a String
+        result = Collections.singletonList(removeSpaces(convertToString(result)));
+        return convertToString(result);
     }
 
-    public String removeSpaces(String data){
+    private String removeSpaces(String data){
 
         String replaced = data.replace("  ", "and");
         replaced = replaced.replace(" ", "");
